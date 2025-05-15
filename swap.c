bf4cc2f8687977f5e3fb43d9622447a03d658f25
samarthhatte <109466822+samarthhatte@users.noreply.github.com>
2025-05-15 08:03:35 +0530

    Actually swap the values

---
+++ swap.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+
+int main(void)
+{
+	int x = 28 ;
+	int y = 50 ;
+	 
+	printf("x is %i\n", x); 
+	printf("y is %i\n", y); 
+	
+	// swapping....
+	int temp = x;
+	x = y;
+	y = temp;
+	
+	
+	printf(x is now%i\n", x);
+	printf(y is now%i\n", y);
+	
+}
